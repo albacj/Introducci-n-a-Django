218a7af25b26f09ec301c9e122975d5948027e9a
Alba Carmona Jamardo <acarmonaj@ayesa.com>
2019-05-16 14:58:18 +0200

    Excepciones

--- polls/urls.py
+++ polls/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 app_name = 'polls'
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<int:question_id>/', views.detail, name = 'detail'),
-    path('<int:question_id>/results/', views.results, name = 'results'),
-    path('<int:question_id>/vote/', views.vote, name = 'vote'),
-    path('specifics/<int:question_id>/', views.detail, name='detail'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.detail, name = 'detail'),
+    path('<int:pk>/results/', views.results, name = 'results'),
+    path('<int:pk>/vote/', views.vote, name = 'vote'),
+    #path('specifics/<int:question_id>/', views.detail, name='detail'),
 ]
